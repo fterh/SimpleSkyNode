beb5356c3a786499b8c02e458c48090b81b5f9ba
Fabian Terh <fterh@Fabians-MacBook-Pro.local>
2018-01-28 10:25:17 +0800

    Added "data status" page to check data freshness

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
+var dataStatus = require('./routes/dataStatus');
 var postLocation = require('./routes/postLocation');
 
 var app = express();
@@ -32,7 +33,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-// app.get('/postlocation', postlocation);
+app.use('/data-status', dataStatus);
 app.use('/postlocation', postLocation);
 
 // catch 404 and forward to error handler
